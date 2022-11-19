3bb05b0121ded27a4f125b8e3dac8a8ba726b5be
LeninNavarro <czymlpnavarro17@gmail.com>
2022-11-19 15:44:04 +0800

    trial component

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,10 +3,11 @@
 import React from "react";
 import herovid from "../assets/video-tv-0819.m4v"
 
-export default Hero(){
+export default function Hero(){
     return (
         <div className="">
         <video autoPlay muted loop src={herovid} className=''></video>
         </div>
     )
 }
+
